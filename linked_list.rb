87b942fee0d6abbb9fc28c8d93754192bde79765
maryfelmendoza <maryfelmendoza@hotmail.com>
2020-05-08 02:45:11 +0000

    method remove added to the challenge linked list

--- linked_list.rb
+++ linked_list.rb
@@ -37,12 +37,27 @@ class LinkedList
     return current
   end
   
-  def add_at(index, item)
-  end
-  
   def remove(index)
+    if @head.nil?
+      puts "the storage is empty, nothing to remove"
+    end
+    
+    if (index==0) 
+      #remove the first element from the list
+      current=@head 
+      #get the element after the head and make head equal to it
+      current.next=new_current 
+      @head=new_current
+    end
+    
+    if (index>0)
+      current= get(index) #(desired node to be removed)
+      before_current= get(index-1) 
+      after_current=current.next
+      before_current.next=after_current
+    end
+      
   end
-  
   
 end
 
@@ -52,11 +67,17 @@ list = LinkedList.new
 
 list.add(3)
 list.add(5)
+list.add(7)
+list.add(9)
 
-list.add_at(1, 11)
-list.add_at(0, 13)
-
+puts list.get(0)
+puts list.get(1)
 puts list.get(2)
+puts list.get(3)
 
+list.remove(2)
 
+puts list.get(0)
+puts list.get(1)
+puts list.get(2)
 puts list.get(3)
